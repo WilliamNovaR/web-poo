9bcf409d092f7efeb89129fd68c46f1caf8fd874
William <williamnova18@gmail.com>
2022-05-14 16:23:18 -0500

    actualizacion 2

---
+++ model/Calificacion.py
@@ -0,0 +1,12 @@
+
+
+class Calificacion:
+
+    def __int__(self) -> None:
+        self.numero_jurados = 0
+        self.id_criterio = ""
+        self.ponderacion = 0
+        self.nota_jurado1 = 0
+        self.nota_jurado2 = 0
+        self.nota_final = 0
+        self.comentario = ""
