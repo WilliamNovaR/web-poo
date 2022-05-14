cfc38b0e82367c66e7b5e7d18cd749c07d9d842d
williamNova18 <100657489+williamNova18@users.noreply.github.com>
2022-05-14 15:32:06 -0500

    probando

--- view/MainView.py
+++ view/MainView.py
@@ -1,58 +1,82 @@
 import streamlit as st
-from pydataxm.pydataxm import ReadDB
+from PIL import Image
 from streamlit_option_menu import option_menu
+from controller.EvalController import EvaluadorController
+from controller.CriterioController import CriterioController
+from controller.ActaController import ActaController
+from view.AboutPartial import consultar_instrucciones
+from view.Evaluar import listar_evaluacion, agregar_evaluacion
+from view.ConfigurarCriterios import seleccionar_opcion
+from view.CrearActa import crearActa
+from view.InicilizarActa import  agregar_datos
+from view.InformacionActas import listar_actas
+from view.AnliticaDato import analisis
 
-from controller.MercadoController import MercadoController
-from model.Consulta import Consulta
-from model.Metrica import Metrica
-from view.AboutPartial import *
+
 
 
 class MainView:
-
     def __init__(self) -> None:
+        self.criterios = []
         super().__init__()
 
+        # Estretagia para manejar el "estado" del controllador y del modelo entre cada cambio de ventana
         if 'main_view' not in st.session_state:
             self.menu_actual = "About"
-            # Inicialización de las variables necesarias
+
+            # Conexión con el controlador
+            self.controller = EvaluadorController()
+            self.criterios_controller = CriterioController()
+            self.actas_controller = ActaController()
 
             st.session_state['main_view'] = self
         else:
-            self.menu_actual = st.session_state.main_view.menu_actual
-             # Carga de las variables necesarias
-        self._inicialializar_layout()
 
-    def _inicialializar_layout(self):
+            # Al exisir en la sesión entonces se actualizan los valores
+            self.menu_actual = st.session_state.main_view.menu_actual
+            self.controller = st.session_state.main_view.controller
+            self.criterios_controller = st.session_state.main_view.criterios_controller
+            self.actas_controller = st.session_state.main_view.actas_controller
+
+        self._dibujar_layout()
+
+    def _dibujar_layout(self):
+        img = Image.open( "C:\\Users\\willi\\Downloads\\streamlit_example_app-main\\streamlit_example_app-main\\view\\puj_logo_vertical_azul_copia.png" )
         # Set page title, icon, layout wide (more used space in central area) and sidebar initial state
-        st.set_page_config(page_title="Análisis mercado energía", page_icon='', layout="wide",
+        st.set_page_config  (page_title="Calificar trabajos finales", page_icon = img, layout="wide",
                            initial_sidebar_state="expanded")
         # Defines the number of available columns del area principal
-        self.col1, self.col2, self.col3 = st.columns([1, 1, 1])
+        self.col1, self.col2, self.col3, self.col4, self.col5, self.col6, self.col7, self.col8 = st.columns([1, 1, 1, 1, 1, 1, 1, 1])
 
         # Define lo que abrá en la barra de menu
         with st.sidebar:
-            self.menu_actual = option_menu("Menu", ["About", '[OtroMenu]Mi Menu'],
-                                           icons=['house', 'gear'], menu_icon="cast", default_index=1)
-
-    def ver_ejemplo(self):
-        pass
+            self.menu_actual = option_menu("Menu", ["About", 'Inicilizar datos actas' , 'Criterios', 'Evaluar nuevo trabajo', 'Calificaciones', 'Acta', 'Resumen actas', 'Estadisticas'],
+                                           icons=['house', 'house' , 'card-list', 'clipboard', 'clipboard-check'], menu_icon="cast", default_index=1)
 
     def controlar_menu(self):
-        # Filtro opciones de menu
         if self.menu_actual == "About":
-            # Welcome message
-            welcome = st.expander(label="Instrucciones", expanded=True)
+            texto = consultar_instrucciones()
+            st.write(texto)
+        elif self.menu_actual == "Inicilizar datos actas":
+            agregar_datos(st, self.controller, self.criterios_controller)
+        elif self.menu_actual == "Criterios":
+            seleccionar_opcion(st, self.criterios_controller)
+        elif self.menu_actual == "Evaluar nuevo trabajo":
+            agregar_evaluacion(st, self.controller, self.criterios_controller)
+        elif self.menu_actual == "Calificaciones":
+            listar_evaluacion(st, self.controller, self.criterios_controller)
+        elif self.menu_actual == "Acta":
+            crearActa(st, self.actas_controller, self.controller)
+        elif self.menu_actual == "Resumen actas":
+            listar_actas( st, self.criterios_controller, self.actas_controller )
+        elif self.menu_actual == 'Estadisticas' :
+            analisis( st, self.controller, self.criterios_controller )
 
-            # Cuando este disponible en pantalla la instruccion de welcome
-            with welcome:
-                st.markdown(mostrar())
-                st.write("")
-        elif self.menu_actual == "[OtroMenu]Mi Menu":
-            self.ver_ejemplo()
+
 
 
 # Main call
+
 if __name__ == "__main__":
-    gui = MainView()
-    gui.controlar_menu()
+    main = MainView()
+    main.controlar_menu()
