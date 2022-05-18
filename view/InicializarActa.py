b78976ac4011058f744f3400e6563b9770b10b99
William <williamnova18@gmail.com>
2022-05-17 23:48:12 -0500

    se mejora los comentarios de codigo y se corrige el numero magico 7

--- view/InicializarActa.py
+++ view/InicializarActa.py
@@ -1,18 +1,18 @@
 from model.EvalEstudiante import EvaluacionEstudiante
 from datetime import datetime
-import matplotlib.font_manager as fm
 
 
 
 
 #inicializa la acta
 def agregar_datos(st, controller):
+    limite_semestre = 7
     mes = int(datetime.today().strftime('%m')) #optiene el mes para saber en que semestre estamos
     st.title("Calificar Trabajos")
     evaluacion_obj = EvaluacionEstudiante()
     evaluacion_obj.id_estudiante = st.text_input("Id estudiante")
     #si el mes es inferior a 7 toma como si fuera el semestre 1 en otro caso lo toma como semestre 2 esto es importante para la acta
-    if mes < 7:
+    if mes < limite_semestre:
         evaluacion_obj.periodo = datetime.today().strftime('%Y') + '-' + '1: '
     else:
         evaluacion_obj.periodo = datetime.today().strftime('%Y') + '-' + '2: '
@@ -29,7 +29,7 @@ def agregar_datos(st, controller):
     evaluacion_obj.nombre_jurado1 = st.text_input("Nombre del jurado1")
     evaluacion_obj.nombre_jurado2 = st.text_input("Nombre del jurado2")
     #pregunta el numero del acta desde el cual se va a empezar
-    if len(controller.evaluaciones) > 0:
+    if len(controller.evaluaciones):
         evaluacion_obj.inicilizar = st.number_input("Numero de acta:", value=controller.evaluaciones[
                                                                                  len(controller.evaluaciones) - 1].inicilizar + 1,
                                                     step=1)#se sugiere el numero siguiente al de la anterior calificacion
@@ -38,6 +38,7 @@ def agregar_datos(st, controller):
     enviado_btn = st.button("Send")
 
     if enviado_btn:
+        #con este for se comprueba que no hayan dos actas inicializadas con el mismo id en caso que si muestra el error y termina la funcion
         for i in controller.evaluaciones:
             if evaluacion_obj.id_estudiante == i.id_estudiante:
                 st.error( "Id repetida" )
